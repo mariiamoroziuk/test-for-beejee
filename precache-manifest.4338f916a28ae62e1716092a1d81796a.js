1b6633be37b46221f7b614ec89c568ec134b8b11
mariiamoroziuk <54170630+mariiamoroziuk@users.noreply.github.com>
2020-02-02 00:24:33 +0200

    Add files via upload

---
+++ precache-manifest.4338f916a28ae62e1716092a1d81796a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4481d0984ab2bf6f85d63b4e4fd72e91",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/index.html"
+  },
+  {
+    "revision": "eb62bd13556eff0600cd",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/static/css/main.5902b626.chunk.css"
+  },
+  {
+    "revision": "668833b65e34c6594b7a",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/static/js/2.8b4deb28.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/static/js/2.8b4deb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb62bd13556eff0600cd",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/static/js/main.89ed1c07.chunk.js"
+  },
+  {
+    "revision": "7dff6e029da8ee575f39",
+    "url": "/mariiamoroziuk.github.io/test-for-beejee/static/js/runtime-main.8855300b.js"
+  }
+]);
